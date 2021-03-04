356146e4868b6645da211e53a3d3ca5214d0838e
Caio Barros de Moura <caiomoura1994@gmail.com>
2021-03-04 01:09:27 -0300

    🚸 improve help text

--- integrations/models.py
+++ integrations/models.py
@@ -5,11 +5,11 @@ class RdStationZapei(models.Model):
     url_sufix = models.CharField(
         max_length=100,
         verbose_name='Sufixo do cliente',
-        help_text="https://zapei-api.herokuapp.com/integracao/'Sufixo do cliente'/"
+        help_text="Acesse https://app.rdstation.com.br/integracoes/webhooks > clique em Criar Webhook > Escolha um nome, no campo GATILHO selecione: Conversão, no campo URL coloque: https://zapei-api.herokuapp.com/integracao/'Sufixo do cliente'/, Eventos de Conversão pode ficar Vazio"
     )
     token_zapei = models.TextField(
         verbose_name='Token Zapei',
-        help_text="Na sua conta Zapei, clique em 'olá...' no canto superior direito, escolha integrações, e clique em Gerar Token"
+        help_text="Acesse https://painel.zapei.com.br/admin/integrations > clique em Gerar Token"
     )
 
     class Meta:
