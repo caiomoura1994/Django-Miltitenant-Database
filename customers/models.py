cc73e17be6605915b282598e9a803f1b3e3c18ca
Caio Moura <caiomoura1994@gmail.com>
2023-06-11 23:18:16 -0300

    update customers null values

--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,6 @@ from tenant_schemas.models import TenantMixin
 
 class Client(TenantMixin):
     name = models.CharField(max_length=100)
-    paid_until = models.DateField()
-    on_trial = models.BooleanField()
+    paid_until = models.DateField(blank=True, default=None, null=True)
+    on_trial = models.BooleanField(blank=True, default=None, null=True)
     created_on = models.DateField(auto_now_add=True)
